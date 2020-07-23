da4caac535839f8fdfbe8f4d9f27a8a7c651d71f
Gwon YoungGwon <youngkwon@croquis.com>
2020-07-23 09:02:44 +0900

    ajax 추가

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { fromEvent } from "rxjs";
 import { map } from "rxjs/operators";
-
+import { ajax } from "rxjs/ajax";
 const $search = document.getElementById("search") as HTMLInputElement;
 
 if ($search) {
   const keyup$ = fromEvent($search, "keyup").pipe(
-    map(event => (event.target as HTMLInputElement).value)
+    map(event => (event.target as HTMLInputElement).value),
+    map(query => ajax.getJSON(`https://api.github.com/search/users?q=${query}`))
   );
+  // observable 반환함
 
   keyup$.subscribe(value => console.log(value));
 }
